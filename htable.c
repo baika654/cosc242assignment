8b69a34ab8292acb56c9ff88360ff3bf8095714f
willmckenzie <54975968+willmckenzie@users.noreply.github.com>
2019-09-11 14:46:03 +1200

    Update htable.c

--- htable.c
+++ htable.c
@@ -4,14 +4,19 @@
 #include "mylib.h"
 #include <string.h>
 
+#define MAX 113
+
+
+
+
 
 /**
-  htable struct, contains variables for:
-  The number of keys currently in the table, the capcity of the table,
-  the contents of the table (an array of strings), the frequencies of each key,
-  a record of how many collisions occur per insertion, and an enum
-  type which dictates the type of hashing method.
- */
+ * htable struct, contains variables for:
+ * The number of keys currently in the table, the capcity of the table,
+ * the contents of the table (an array of strings), the frequencies of each key,
+ * a record of how many collisions occur per insertion, and an enum
+ * type which dictates the type of hashing method.
+*/
 struct htablerec{
     int numKeys;
     int capacity;
@@ -20,32 +25,6 @@ struct htablerec{
     int *stats;
     hashing_t method;
 };
-
-/**
-  This method creates and returns a new htable struct.
-  It sets all the variables of the new htable struct to their default values
-  (NULL or 0),  and allocates memory to all the arrays and the object itself.
-
-  The hashing_t method paramater is to determine what hashing method to use
-  for the new table.
-
- */
-htable htable_new(int size, hashing_t method){
-    int i; 
-    htable result = emalloc(sizeof result);
-    result->numKeys = 0;
-    result->capacity = size;
-    result->method = method;
-    result->items = emalloc(size * (sizeof result->items[0]));
-    result->frequencies = emalloc(size * sizeof result->frequencies[0]);
-    result->stats = emalloc(size * sizeof result->stats[0]);
-    for(i = 0; i < size; i++){
-        result->items[i] = NULL;
-        result->frequencies[i] = 0;
-        result->stats[i] = 0;
-    }
-    return result;
-}
 
 
 /**
@@ -64,7 +43,7 @@ static void print_stats_line(htable h, FILE *stream, int percent_full) {
     int at_home = 0;
     int max_collisions = 0;
     int i = 0;
-    
+
     if (current_entries > 0 && current_entries <= h->numKeys) {
         for (i = 0; i < current_entries; i++) {
             if (h->stats[i] == 0) {
@@ -113,19 +92,46 @@ void htable_print_stats(htable h, FILE *stream, int num_stats) {
     fprintf(stream, "------------------------------------------------------\n\n");
 }
 
+
+
 /**
-  This method first frees all the keys in the keys array of the htable.
-  Then it frees all the arrays in the htable, and finally frees the object
-  itself.
+ * This method creates and returns a new htable struct.
+ * It sets all the variables of the new htable struct to their default values
+ * (NULL or 0),  and allocates memory to all the arrays and the object itself.
+ * The hashing_t method paramater is to determine what hashing method to use
+ * for the new table.
+ *
+ * @param size the desired size/capacity of the hash table
+ * @param method the desired hashing method to be used, either linear or
+ * double.
+ *
+ * @return result the resulting htable that has been created.
+*/
+htable htable_new(int size, hashing_t method){
+    int i; 
+    htable result = emalloc(sizeof result);
+    result->numKeys = 0;
+    result->capacity = size;
+    result->method = method;
+    result->items = emalloc(size * (sizeof result->items[0]));
+    result->frequencies = emalloc(size * sizeof result->frequencies[0]);
+    result->stats = emalloc(size * sizeof result->stats[0]);
+    for(i = 0; i < size; i++){
+        result->items[i] = NULL;
+        result->frequencies[i] = 0;
+        result->stats[i] = 0;
+    }
+    return result;
+}
 
-  //NOTE: Currently if free is called on h->items, it results in an error
-  message: double free or corruption (out)
-           Aborted (core dumped)
-  Should probably address this as it may cause a memory leak.
-  
-  Currently the free call on h->items is commented out.
 
- */
+/**
+ * This method first frees all the keys in the keys array of the htable.
+ * Then it frees all the arrays in the htable, and finally frees the object
+ * itself.
+ *
+ * @param h the hash table to be freed.
+*/
 void htable_free(htable h){
     int i;
     for(i = 0; i < h->capacity; i++){
@@ -138,23 +144,29 @@ void htable_free(htable h){
 }
 
 /**
-  This method prints all the keys of the htable to a given output stream.
-
- */
+ * This method prints all the keys of the htable to a given output stream.
+ *
+ * @param h the htable that we want to print the keys from.
+ * @param stream the output stream that we want to print to.
+*/
 void htable_print(htable h, FILE *stream){
     int i;
     for(i = 0; i < h->capacity; i++){
         if(h->items[i] != NULL){
-            fprintf(stream, "%d,  %s\n", h->frequencies[i], h->items[i]);
+            fprintf(stream, "%d    %s\n", h->frequencies[i], h->items[i]);
         }
     }
 }
 
 /**
-  This static method converts a string to an unsigned int.
-  This is used for inserting and searching indeces in the htable.
-
- */
+ * This static method converts a string to an unsigned int.
+ * This is used for inserting and searching indeces in the htable.
+ *
+ * @param word the string to be converted to an unsigned int.
+ *
+ * @return result the resulting unsigned int after the string
+ * has been converted.
+*/
 static unsigned int wordToInt(char *word){
     unsigned int result = 0;
     while(*word != '\0'){
@@ -164,36 +176,41 @@ static unsigned int wordToInt(char *word){
 }
 
 /**
-  This static method allocates memory for a string and inserts it
-  at a given index in an htable.
-  This method makes the insertion code A LOT cleaner.
+ * This static method allocates memory for a string and inserts it
+ * at a given index in an htable.
+ * This method makes the insertion code a lot cleaner.
+ *
+ * @param h the htable that the string is inserted at.
+ * @param word the string to be inserted.
+ * @param key the index to insert the string at.
 */
 static void htableInsertAt(htable h, char *word, int key){
-    h->items[key] = emalloc(sizeof h->items[key][0] * strlen(word) + 1);
+    h->items[key] = emalloc(sizeof h->items[key][0] * strlen(word)+1);
     strcpy(h->items[key], word);
     h->frequencies[key]++;
 }
 
 
 /**
-  This method uses linear hashing to insert a key into an htable.
-  It iterates based on the linear probing algorithm until it finds either
-  a free cell, a matching string, or has iterated through the entire table.
-
-  If it finds a free cell, it inserts the given string, increases the numKeys
-  variable of the htable, and sets the stats[numKeys] to the number of
-  collisions that occured during insertion.
-
-  If it finds a matching string, it increases the key's matching frequency.
-
-  If it iterates through through the whole table and doesn't find either
-  stopping case, it returns -1 (insertion fail).
-
+ * This method uses linear hashing to insert a key into an htable.
+ * It iterates based on the linear probing algorithm until it finds either
+ * a free cell, a matching string, or has iterated through the entire table.
+ * If it finds a free cell, it inserts the given string, increases the numKeys
+ * variable of the htable, and sets the stats[numKeys] to the number of
+ * collisions that occured during insertion.
+ * If it finds a matching string, it increases the key's matching frequency.
+ * If it iterates through through the whole table and doesn't find either
+ * stopping case, it returns -1 (insertion fail).
+ *
+ * @param h the htable that the key is to be inserted into.
+ * @param word the string to be inserted into the htable.
 */
 static int linearInsert(htable h, char *word){
+ 
     int collisions = 0;
     int key = wordToInt(word) % h->capacity;
-    
+
+  
     if(h->items[key] == NULL){
         htableInsertAt(h, word, key);
         h->stats[h->numKeys++] = collisions;
@@ -221,31 +238,25 @@ static int linearInsert(htable h, char *word){
 }
 
 /**
-  This method calculates the next step for double hashing
-
-  @param h the hash table whose capacity field will be used to calculate the
-  next key
-  @param i_key the key to iterate.
+ * This method calculates the next step for double hashing.
+ *
+ * @param h the htable 
 */
 static unsigned int htable_step(htable h, unsigned int i_key){
     return 1 + (i_key % (h->capacity - 1));
 }
 
-/**
+/*
   This method uses double hashing to insert a key into a hash table.
   It iterates based on the double hashing algorithm until it finds either
   a free cell, a matching string, or has iterated through the entire table.
-
   If it finds a free cell, it inserts the given string, increases the numKeys
   variable of the htable, and sets the stats[numKeys] to the number of
   collisions that occured during insertion.
-
   If it finds a matching string, it increases the key's matching frequency.
-
   If it iterates through through the whole table and doesn't find either
   stopping case, it returns -1 (insertion fail).
-
- */
+*/
 static int doubleInsert(htable h, char *word){
     int collisions = 0;
     int key = wordToInt(word) % h->capacity;
@@ -276,16 +287,13 @@ static int doubleInsert(htable h, char *word){
 }
 
 
-/**
+/*
   This method uses double hashing to search for a key into a hash table.
   It iterates based on the double hashing algorithm until it finds either
   a free cell, a matching string, or has iterated through the entire table.
-
   If it finds a free cell, it returns a 0, indicating that the
   word is not in the table.
-
   If it finds a matching string, it increases the key's matching frequency.
-
   If it iterates through through the whole table and doesn't find either
   stopping case, it returns 0 (word is not in the table).
 */
@@ -306,30 +314,27 @@ static int doubleSearch(htable h, char *word){
     }
 }
 
-/**
+/*
   This method uses double hashing to search for a key into a hash table.
   It iterates based on the linear probing algorithm until it finds either
   a free cell, a matching string, or has iterated through the entire table.
-
   If it finds a free cell, it returns a 0, indicating that the
   word is not in the table.
-
   If it finds a matching string, it increases the key's matching frequency.
-
   If it iterates through through the whole table and doesn't find either
   stopping case, it returns 0 (word is not in the table).
 */
 static int linearSearch(htable h, char *key){
     int collisions = 0;
     int pos = wordToInt(key) % h->capacity;
-    while(strcmp(key, h->items[pos]) != 0 && collisions <= h->capacity){
-        pos = (pos + 1 % h->capacity);
+
+    /* printf("Searching for  %s\n", key); */
+    while(h->items[pos] != NULL && (strcmp(key, h->items[pos]) != 0) &&  pos < h->capacity){
+        pos = ((pos + 1) % h->capacity);
         collisions++;
-        if(h->items[pos] == NULL){
-            return 0;
-        }
+       
     }
-    if(collisions >= h->capacity){
+    if(collisions == h->capacity){
         return 0;
     }else{
         return h->frequencies[pos];
@@ -338,20 +343,12 @@ static int linearSearch(htable h, char *key){
 }
 
 
-/**
+/*
   This method uses either the linearInsert method or the doubleInsert method
   to insert a word into a given hash table h, based on that hash table's
   method variable (LINEAR_P for linear probing, DOUBLE_H for double hashing).
-
- */
+*/
 int htable_insert(htable h, char *word){
-    /*This block checks if the table being inserted into is full.
-      If it is, return -1 (the return code for insertion fail)
-     */
-    if(h->numKeys >= h->capacity){
-        return -1;
-    }
-    
     if(h->method == LINEAR_P){
         return linearInsert(h, word);
     }else{
@@ -359,11 +356,10 @@ int htable_insert(htable h, char *word){
     }
 }
 
-/**
+/*
   This method uses either the linearInsert method or the doubleInsert method
   to search for a word into a given hash table h, based on that hash table's
   method variable (LINEAR_P for linear probing, DOUBLE_H for double hashing).
-
 */
 int htable_search(htable h, char *word){
     if(h->method == LINEAR_P){
@@ -373,6 +369,13 @@ int htable_search(htable h, char *word){
     }
 }
 
+/*
+ *This function prints the entire contents of the hash table. Each element of 
+ *the has table is printed one line at a time. 
+ *
+ * @param h the hashtable to print entire contents from.
+ *
+ */
 
 void htable_print_entire_table(htable h) {
     int i;
